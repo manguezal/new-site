dbdc87ed5cd9f51b48cd51f308f0281a1695be7e
Leonardo Almeida <leonardoalmeidamarques@gmail.com>
2017-04-06 02:02:49 -0300

    adição de comportamentos no Languageswitcher e configuração de testes

--- js/components/LanguageSwitcher.js
+++ js/components/LanguageSwitcher.js
@@ -2,16 +2,35 @@ import React, {Component} from 'react';
 
 
 export default class LanguageSwitcher extends Component{
-  
+   
+   constructor(props) {
+    super(props);
+    this.state = {isPortuguese: true};
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick() {
+    this.setState(prevState => ({
+      isPortuguese: !prevState.isPortuguese
+    }));
+  };
+
+  toggleState(){
+    let cssClass = "toggler ";
+    cssClass += (this.state.isPortuguese) ? 'toggler-is-portuguese' : 'toggler-is-english';
+    
+    return cssClass;
+  }
+
   render(){
     return (
           <div className="language-switcher-wrapper">
-            <span className="language-switcher">
-              <span className="toggler"></span>
+            <span className="language-switcher" onClick={this.handleClick}>
+              <span className={this.toggleState()}></span>
               <span className="lang lang-pt">Por</span>
               <span className="lang lang-en">Eng</span>
             </span>
           </div>
-    )
-  }
+    );
+  };
 }
